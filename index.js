f5aa5864993fdb05395533238c44a70f11093e03
unicorn808 <unicorncalcs@gmail.com>
2017-09-18 21:21:53 -1000

    Added Quizlet import - still needs some help though

--- index.js
+++ index.js
@@ -1,128 +1,3 @@
-           var question = "What is the capital of {0}?";
-            var terms = [
-                {
-                    term: "Afghanistan",
-                    definition: "Kabul"
-                }, {
-                    term: "Australia",
-                    definition: "Canberra"
-                }, {
-                    term: "Brazil",
-                    definition: "Brasilia"
-                }, {
-                    term: "China",
-                    definition: "Beijing"
-                }, {
-                    term: "Cote D'lvoire",
-                    definition: "Yamoussoukro"
-                }, {
-                    term: "Cuba",
-                    definition: "Havana"
-                }, {
-                    term: "Democratic Republic Of Congo",
-                    definition: "Kinshasa"
-                }, {
-                    term: "Dominican Republic",
-                    definition: "Santo Domingo"
-                }, {
-                    term: "Egypt",
-                    definition: "Cairo"
-                }, {
-                    term: "Ethiopia",
-                    definition: "Addis Ababa"
-                }, {
-                    term: "France",
-                    definition: "Paris"
-                }, {
-                    term: "Germany",
-                    definition: "Berlin"
-                }, {
-                    term: "Greece",
-                    definition: "Athens"
-                }, {
-                    term: "Guatemala",
-                    definition: "Guatemala City"
-                }, {
-                    term: "Haiti",
-                    definition: "Port-au-Prince"
-                }, {
-                    term: "India",
-                    definition: "New Delhi"
-                }, {
-                    term: "Indonesia",
-                    definition: "Jakarta"
-                }, {
-                    term: "Iran",
-                    definition: "Tehran"
-                }, {
-                    term: "Iraq",
-                    definition: "Baghdad"
-                }, {
-                    term: "Ireland",
-                    definition: "Dublin"
-                }, {
-                    term: "Japan",
-                    definition: "Tokyo"
-                }, {
-                    term: "Libya",
-                    definition: "Tripol"
-                }, {
-                    term: "Malaysia",
-                    definition: "Kuala Lumpur"
-                }, {
-                    term: "Morocco",
-                    definition: "Rabat"
-                }, {
-                    term: "North Korea",
-                    definition: "Pyongyang"
-                }, {
-                    term: "Pakistan",
-                    definition: "Islamabad"
-                }, {
-                    term: "Papua New Guinea",
-                    definition: "Port Moresby"
-                }, {
-                    term: "Peru",
-                    definition: "Lima"
-                }, {
-                    term: "Philippines",
-                    definition: "Manila"
-                }, {
-                    term: "Russia",
-                    definition: "Moscow"
-                }, {
-                    term: "Sierre Leone",
-                    definition: "Freetown"
-                }, {
-                    term: "South Korea",
-                    definition: "Seoul"
-                }, {
-                    term: "Spain",
-                    definition: "Madrid"
-                }, {
-                    term: "Syria",
-                    definition: "Damascus"
-                }, {
-                    term: "Thailand",
-                    definition: "Bangkok"
-                }, {
-                    term: "Tunisia",
-                    definition: "Tunis"
-                }, {
-                    term: "Turkey",
-                    definition: "Ankara"
-                }, {
-                    term: "Ukraine",
-                    definition: "Kiev"
-                }, {
-                    term: "Venezuela",
-                    definition: "Caracas"
-                }, {
-                    term: "Yemen",
-                    definition: "Sana'a"
-                }
-            ];
-
             var questions = [];
             var currentQuestion = {};
             function maskPhrase(word, percent) {
@@ -150,73 +25,76 @@
                 }
 
                 currentQuestion = questions.shift();
-
-                $(".mid span").parent().width(60 * currentQuestion.answer.length);
-                $(".mid h1").text(currentQuestion.question);
-                $(".mid span").text(maskPhrase(currentQuestion.answer, currentQuestion.difficulty));
-                $(".mid input").val('');
-                $(".mid span").animate({
-                    color: "#D3D3D3"
-                }, 100);
-            }
-
-            function endQuestion() {
-                $('.mid input').toggle();
-                $('.mid input').focus();
-
-                var txt = $(".mid input").val();
-
-                if (txt.toLowerCase() == currentQuestion.answer.toLowerCase()) {
-                    currentQuestion.difficulty += 0.2;
-
-                    if (currentQuestion.difficulty <= 1.4) {
-                        questions.push(currentQuestion);
-                    }
-                } else {
-                    currentQuestion.difficulty -= 0.2;
-                    questions.unshift(currentQuestion);
-                }
-
-                showQuestion();
-            }
-
-
-            $(".mid input").keyup(function (e) {
-                var txt = $(e.target).val();
-                
-                if (txt.length >= currentQuestion.answer.length) {
-                    if (txt.toLowerCase() == currentQuestion.answer.toLowerCase()) {
-                        $(e.target).toggle();
-
-                        $(".mid span").animate({
-                            color: "green"
-                        }, 200);
-
-                        setTimeout(endQuestion, 400);
-                    } else {
-                        $(e.target).toggle();
-
-                        $(".mid span").text(currentQuestion.answer);
-                        $(".mid span").animate({
-                            color: "red"
-                        }, 200);
-
-                        setTimeout(endQuestion, 1500);
-                    }
-                }
-            });
-           function setDifficulty(diff) {
-		   	questions = [];
-            		currentQuestion = {};
-           		for (var i = 0; i < terms.length; i++) {
-                	var t = terms[i];
-                	questions.push({
-                   	question: question.format(t.term),
-                    	answer: t.definition.toLowerCase(),
-                    	difficulty: diff
-                });
-           		}
-		   showQuestion();
+                try{
+                    $(".mid span").parent().width(60 * currentQuestion.answer.length);
+                    $(".mid h1").text(currentQuestion.question);
+                    $(".mid span").text(maskPhrase(currentQuestion.answer, currentQuestion.difficulty));
+                    $(".mid input").val('');
+                    $(".mid span").animate({
+                        color: "#D3D3D3"
+                    }, 100);
+                } catch(e) {
+                   $('#new-terms').modal('open');
+               }
            }
-	setDifficulty(0.0);
+
+           function endQuestion() {
+            $('.mid input').toggle();
+            $('.mid input').focus();
+
+            var txt = $(".mid input").val();
+
+            if (txt.toLowerCase() == currentQuestion.answer.toLowerCase()) {
+                currentQuestion.difficulty += 0.2;
+
+                if (currentQuestion.difficulty <= 1.4) {
+                    questions.push(currentQuestion);
+                }
+            } else {
+                currentQuestion.difficulty -= 0.2;
+                questions.unshift(currentQuestion);
+            }
+
+            showQuestion();
+        }
+
+
+        $(".mid input").keyup(function (e) {
+            var txt = $(e.target).val();
+            
+            if (txt.length >= currentQuestion.answer.length) {
+                if (txt.toLowerCase() == currentQuestion.answer.toLowerCase()) {
+                    $(e.target).toggle();
+
+                    $(".mid span").animate({
+                        color: "green"
+                    }, 200);
+
+                    setTimeout(endQuestion, 400);
+                } else {
+                    $(e.target).toggle();
+
+                    $(".mid span").text(currentQuestion.answer);
+                    $(".mid span").animate({
+                        color: "red"
+                    }, 200);
+
+                    setTimeout(endQuestion, 1500);
+                }
+            }
+        });
+        function setDifficulty(diff) {
+          questions = [];
+          currentQuestion = {};
+          for (var i = 0; i < terms.length; i++) {
+             var t = terms[i];
+             questions.push({
+                question: question.format(t.term),
+                answer: t.definition.toLowerCase(),
+                difficulty: diff
+            });
+         }
+         showQuestion();
+     }
+     setDifficulty(0.0);
 
